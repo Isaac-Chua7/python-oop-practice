ce29cdb789babfbd60ee115801f6938e8af23cd5
Isaac-Chua7 <a1969124@adelaide.edu.au>
2026-07-13 11:08:27 +0930

    Class variables Implemented
    Class variables Implemented  (minutes 12-20 of bro code python OOP course)

--- pythonOOP/pet.py
+++ pythonOOP/pet.py
@@ -1,9 +1,14 @@
 class Pet:
+
+    num_pets = 0
+    is_Animal = True
+
     def __init__(self, species, name, age, plays_fetch):
         self.species = species
         self.name = name
         self.age = age
         self.plays_fetch = plays_fetch
+        Pet.num_pets += 1
     
     def descibe(self):
         print(f"Hello my name is {self.name}, I am a {self.age} year old {self.species}")
