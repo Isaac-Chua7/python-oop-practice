23a59c1222faecd5aeb3f692fc015114fdebb3a9
Isaac-Chua7 <a1969124@adelaide.edu.au>
2026-07-17 08:43:40 +0930

    Learned how to use super function
    Learned how to use super function from mins 43-55 of Bro Code python OOP course.

---
+++ pythonOOP/super.py
@@ -0,0 +1,36 @@
+
+
+class Shape():
+    def __init__(self, colour, is_regular):
+        self.colour = colour
+        self.is_regular = is_regular
+
+    def describe(self):
+        print(f"I am {self.colour} and I am {"a regular polygon" if self.is_regular else "an irregular polygon"}")
+    
+class Square(Shape):
+    def __init__(self, colour, is_regular, width):
+        super().__init__(colour, is_regular)
+        self.width = width
+
+    def describe(self):
+        super().describe()
+        print(f"I also have an area of {self.width*self.width} cm^2")
+
+
+class Trapezium(Shape):
+    def __init__(self, colour, is_regular, height, top_length, bottom_length):
+        super().__init__(colour, is_regular)
+        self.height = height
+        self.top_length = top_length
+        self.bottom_length = bottom_length
+    
+    def describe(self):
+        super().describe()
+        print(f"I also have an area of {(self.top_length+self.bottom_length)/2*self.height} cm^2")
+
+square = Square("red", True, 6.7)
+square.describe()
+
+trap = Trapezium("blue", True, height=10, top_length=12, bottom_length=3)
+trap.describe()
