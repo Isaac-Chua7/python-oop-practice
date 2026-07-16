b93e8c88f5ebba0170762556f92df553fcc64525
Isaac-Chua7 <a1969124@adelaide.edu.au>
2026-07-16 18:39:57 +0930

    Implemented Multiple Inheritance
    Implemented Multiple Inheritance (BroCode course mins 27-34)

--- pythonOOP/athlete.py
+++ pythonOOP/athlete.py
@@ -1,4 +1,6 @@
-class Athlete:
+from extra_classes import Human, Offensive_Specialist, Defensive_Specialist
+
+class Athlete(Human):
     def __init__(self, name, sport, height):
         self.name = name
         self.sport = sport
@@ -7,15 +9,15 @@ class Athlete:
     def introduce(self):
         print(f"Hi my name is {self.name}, I play {self.sport} and stand at {self.height} cm")
     
-class Basketballer(Athlete):
+class Basketballer(Athlete, Defensive_Specialist):
     def score(self):
         print(f"{self.name} steps back and launches from 3")
 
-class Volleyballer(Athlete):
+class Volleyballer(Athlete, Offensive_Specialist):
     def score(self):
         print(f"{self.name} rises and spikes the ball down")
 
-class Soccerer(Athlete):
+class Soccerer(Athlete, Offensive_Specialist):
     def score(self):
         print(f"{self.name} bends it like Beckham around the keeper")
 
